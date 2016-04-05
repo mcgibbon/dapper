cec57f33e6758cf73cedf44ed57a1f3e636d3a05
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2016-04-05 14:26:48 -0700

    added zlcl_from_T_RH

--- dapper/_dapper/util.py
+++ dapper/_dapper/util.py
@@ -4,4 +4,13 @@ Created on Thu Mar 24 12:18:26 2016
 
 @author: mcgibbon
 """
+from xarray.ufuncs import log as xarray_log
 
+
+def zlcl_from_T_RH(T, RH):
+    g0 = 9.80665  # standard gravitational acceleration (m/s)
+    Cpd = 1005.  # Specific heat of dry air at constant pressure (J/kg/K)
+    Gammad = g0/Cpd  # Dry adabatic lapse rate (K/m)
+    Tlcl = 1./((1./(T-55.))-(xarray_log(RH/100.)/2840.)) + 55.
+    zlcl = (T - Tlcl)/Gammad
+    return zlcl
