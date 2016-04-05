c5143453218da51f78a7e526be540438cdbce8d9
Jeremy McGibbon <mcgibbon@atmos.washington.edu>
2016-04-05 14:23:40 -0700

    updated __init__ file export behavior

--- dapper/__init__.py
+++ dapper/__init__.py
@@ -6,9 +6,12 @@ Created on Thu Mar 24 11:02:22 2016
 """
 __all__ = []
 
-def _export(defn):
-    globals()[defn.__name__] = defn
-    __all__.append(defn.__name__)
+
+def export(defn, as_name=None):
+    if as_name is None:
+        as_name = defn.__name__
+    globals()[as_name] = defn
+    __all__.append(as_name)
     return defn
 
 from ._dapper import *
--- dapper/_dapper/__init__.py
+++ dapper/_dapper/__init__.py
@@ -4,14 +4,16 @@ Created on Thu Mar 24 11:02:22 2016
 
 @author: mcgibbon
 """
-from .. import _export as package_export
+from .. import export as package_export
 __all__ = []
 
 
-def export(defn):
-    globals()[defn.__name__] = defn
-    __all__.append(defn.__name__)
-    defn = package_export(defn)
+def export(defn, as_name=None):
+    if as_name is None:
+        as_name = defn.__name__
+    globals()[as_name] = defn
+    __all__.append(as_name)
+    defn = package_export(defn, as_name)
     return defn
 
 from . import dataset, magic, retrieval, time, util
